04a3436420585529e4444636b78a91f8127371c2
Mikhail Ignatov <ignatov.m55@gmail.com>
2024-04-19 14:10:00 +0600

    Второй коммит

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
+  entry: 'index.js',
   output: {
-    filename: 'my-first-webpack.bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
   },
+  plugins: [new HtmlWebpackPlugin()],
+};
+
+module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
 };
+
